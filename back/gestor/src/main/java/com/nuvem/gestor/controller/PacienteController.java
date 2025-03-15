3e028c6aa9973670fc0000911033047141c9b3bf
itsmepaiva <saviopaivajr@gmail.com>
2025-03-15 20:28:03 +0000

    correçao request body spring

--- back/gestor/src/main/java/com/nuvem/gestor/controller/PacienteController.java
+++ back/gestor/src/main/java/com/nuvem/gestor/controller/PacienteController.java
@@ -4,19 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.nuvem.gestor.domain.Paciente;
 import com.nuvem.gestor.domain.DTO.PacienteDTO;
 import com.nuvem.gestor.service.PacienteService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/pacientes")
